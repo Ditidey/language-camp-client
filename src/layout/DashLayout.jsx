6902963181f2d0ecd71b53a780780f552d848d7e
Ditidey <ditidey2017@gmail.com>
2023-06-07 23:59:07 +0100

    manage classes updated

--- src/layout/DashLayout.jsx
+++ src/layout/DashLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import useTitle from '../components/hooks/useTitle';
+import { FaBook, FaHome, FaSchool, FaUserCheck, FaUsers } from 'react-icons/fa';
 
 const DashLayout = () => {
     useTitle('Dashboard')
@@ -10,9 +11,10 @@ const DashLayout = () => {
         <div className='px-10'>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">                   
+                <div className="drawer-content flex flex-col items-center justify-center">    
+                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>               
                     <Outlet></Outlet>
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    
 
                 </div>
                 <div className="drawer-side bg-purple-900">
@@ -21,8 +23,9 @@ const DashLayout = () => {
                          
                        {
                         isAdmin && <>
-                         <li><NavLink to='/dash/class-manage' className={({isActive})=> isActive ? 'text-blue-400' : ''}>Manage Classes</NavLink></li>
-                         <li><NavLink to='/dash/manage-user' className={({isActive})=> isActive ? 'text-blue-400' : ''}>Manage Users</NavLink></li>
+                         <li><NavLink to='/dash/admin' className={({isActive})=> isActive ? 'text-blue-400' : ''}><FaHome></FaHome>Admin Dashboard</NavLink></li>
+                         <li><NavLink to='/dash/class-manage' className={({isActive})=> isActive ? 'text-blue-400' : ''}><FaSchool></FaSchool> Manage Classes</NavLink></li>
+                         <li><NavLink to='/dash/manage-user' className={({isActive})=> isActive ? 'text-blue-400' : ''}><FaUsers></FaUsers>Manage Users</NavLink></li>
                         </>
                        }
                        {
@@ -33,10 +36,11 @@ const DashLayout = () => {
                         </>
                        }
 
-                        <div className='divider bg-white'></div>
-                        <li><NavLink to='/' className={({isActive})=> isActive ? 'text-blue-400' : ''}>Home</NavLink></li>
-                        <li><NavLink to='/classes' className={({isActive})=> isActive ? 'text-blue-400' : ''}>Classes</NavLink></li>
-                        <li><NavLink to='/instructors' className={({isActive})=> isActive ? 'text-blue-400' : ''}>Instructors</NavLink></li>
+                       
+                        <hr className='border-purple-50 my-6'/>
+                        <li><NavLink to='/' className={({isActive})=> isActive ? 'text-blue-400' : ''}><FaHome></FaHome>Home</NavLink></li>
+                        <li><NavLink to='/classes' className={({isActive})=> isActive ? 'text-blue-400' : ''}><FaBook></FaBook>Classes</NavLink></li>
+                        <li><NavLink to='/instructors' className={({isActive})=> isActive ? 'text-blue-400' : ''}><FaUserCheck></FaUserCheck>Instructors</NavLink></li>
                         <button className='bg-purple-50 p-5 text-blue-900 uppercase mt-10'>Log out</button>
                         
                     </ul>
--- src/pages/AdminPages/ClassManage.jsx
+++ src/pages/AdminPages/ClassManage.jsx
@@ -5,22 +5,24 @@ import { FaEdit } from 'react-icons/fa';
 const ClassManage = () => {
     const [classes] = getClasses();
     return (
-        <div className='w-full px-20 py-10'>
-            <hr className='w-1/2 ms-56 border-purple-600' />
-            <p className='text-center text-3xl font-serif my-2 '> Your added {classes.length} classes</p>
-            <hr className='w-1/2 ms-56 border-purple-600 mb-10' />
+        <div className='w-full px-5 py-10 overflow-x-auto'>
+            <hr className='w-1/3 ms-96 border-purple-600' />
+            <p className='text-center text-3xl font-serif my-2 '> {classes.length} classes total</p>
+            <hr className='w-1/3 ms-96 border-purple-600 mb-10' />
             <div className="overflow-x-auto">
-                <table className="table table-pin-rows table-pin-cols">
-                    <thead>
+                <table className="table table-pin-rows table-pin-cols  ">
+                    <thead className='bg-purple-50 overflow-x-auto'>
                         <tr className='text-xl text-purple-950 font-mono bg-purple-50'>
                             <td>Image</td>
                             <td>Class name</td>
-                            <td>Fee</td>
+                            <td>Teacher Name</td>
+                            <td>Teacher Email</td>
                             <td>Seat</td>
-                            <td>Students</td>
+                            <td>Fee</td>
                             <td>Status</td>
-                            <td>Feedback</td>
-                            <td>Edit</td>
+                            <td>Approved</td>
+                            <td>Denied</td>
+                            <td>FeedBack</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -30,12 +32,15 @@ const ClassManage = () => {
                                     <img src={each.class_image} />
                                 </div>  </th>
                                 <td>{each.class_name}</td>
-                                <td className='text-left'>${each.fee}</td>
+                                <td>{each.teacher_name}na</td>
+                                <td>{each.teacher_email}em</td>
                                 <td className='text-center'>{each.seat}</td>
-                                <td className='text-center'>0</td>
-                                <td>{each.status}</td>
-                                <td>no</td>
-                                <th><button className='btn bg-purple-800 text-blue-100'><FaEdit className='text-purple-100'></FaEdit>Edit</button></th>
+                                <td className='text-left'>${each.fee}</td> 
+                                <td>{each.status}</td>                              
+                                <td className='text-center'>0</td>                                 
+                                <th><button className='btn bg-purple-800 text-blue-100'> Approved</button></th>
+                                <th><button className='btn bg-purple-800 text-blue-100'> Deny</button></th>
+                                <th><button className='btn bg-purple-800 text-blue-100'><FaEdit className='text-purple-100'></FaEdit>Feedback</button></th>
                             </tr>)
                         }
                     </tbody>
